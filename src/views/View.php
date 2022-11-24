6210c21b8c1acc06af5b2f63b5c98a9ae686bd6b
toxicbloud <fanta.r55000@gmail.com>
2022-11-24 17:09:13 +0100

    Update View.php

--- src/views/View.php
+++ src/views/View.php
@@ -27,31 +27,6 @@ class View
     public function setTemplate($content, $titre, Request $rq)
     {
         $path = $rq->getUri()->getBasePath();
-        $categories = <<<END
-        <li class="nav-item d-flex">
-            <a class="nav-link" href="$path/cocktails">Listes publiques</a>
-        </li>
-    </ul>
-    <a class="nav-link" href="$path/login">Connexion</a>
-END;
-        if (Authentication::isConnected()) {
-            $nom = $_SESSION['user']->username;
-            $categories = <<<END
-                <li class="nav-item d-flex">
-                    <a class="nav-link" href="$path/cocktails">Cocktails</a>
-                </li>
-                <!-- <li class="nav-item d-flex">
-                    <a class="nav-link" href="$path/creerliste">Creer votre liste</a>
-                </li>
-                <li class="nav-item d-flex">
-                    <a class="nav-link" href="$path/meslistes">Mes Listes</a>
-                </li> -->
-            </ul>
-
-            <span class="navbar-brand">$nom</span>
-            <a class="nav-link" href="$path/logout">Deconnexion</a>
-END;
-        }
         $temp = <<<END
          <!DOCTYPE html> <html>
           <head>
@@ -65,26 +40,58 @@ END;
             <meta name="theme-color" content="#F04823">
            <link rel="icon" href="$path/Photos/favicon.ico" />
            <title> $titre </title>
-           <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
-            <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+           
+           <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
+            <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+            <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css">
+            <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
 
            <link href="$path/css/style.css" rel="stylesheet" >
           </head>
         <body>
-        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-	        <a class="navbar-brand" href="$path">
-		        <img src="$path/Photos/favicon.ico" alt="" width="60" height="60">
-	        </a>
-	        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-		        <span class="navbar-toggler-icon"></span>
-	        </button>
-
-	        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-		        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                        $categories
-
-	        </div>
-        </nav>
+            <nav class="navbar navbar-expand-lg bg-light">
+                <div class="container-fluid">
+                    <a class="navbar-brand" href="$path">
+                    <img src="$path/Photos/favicon.ico" alt="" width="60" height="60">
+                    </a>
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                            <li class="nav-item dropdown">
+                            <!--<a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                Dropdown
+                            </a>
+                            <ul class="dropdown-menu">
+                                <li><a class="dropdown-item" href="#">Action</a></li>
+                                <li><a class="dropdown-item" href="#">Another action</a></li>
+                                <li><hr class="dropdown-divider"></li>
+                                <li><a class="dropdown-item" href="#">Something else here</a></li>
+                            </ul> -->
+                            </li>
+                            <li class="nav-item">
+                            <a class="nav-link" href="$path/cocktails">Cocktails</a>
+                            </li>
+                        </ul>
+                        <div class="navbar-nav justify-content-end">
+END;
+        if (Authentication::isConnected()) {
+            $nom = $_SESSION['user']->username;
+            $temp .= <<<END
+                        <span class="navbar-brand">$nom</span>
+                        <a class="nav-link" href="$path/logout">Deconnexion</a>
+END;
+        } else {
+            $temp .= <<<END
+                        <a class="nav-link" href="$path/login">Connexion</a>
+END;
+        }
+        $temp .= <<<END
+                        </div>
+                    </div>
+                </div>
+            </nav>
           $content
          </body>
         <html>
