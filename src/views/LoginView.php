4d177777a17cfc063078dc6a662a6f22c47a7f5b
clement <clement.joly3@etu.univ-lorraine.fr>
2022-11-29 22:48:12 +0100

    update loginView

--- src/views/LoginView.php
+++ src/views/LoginView.php
@@ -20,22 +20,47 @@ class LoginView
     {
         $path = $this->rq->getUri()->getBasePath();
         $temp = <<<END
-        <body class="text-center">
-        <form class="form-signin" action="login" method="post">
-            <h1 class="h3 mb-3 font-weight-normal">Connexion</h1>
-            <label for="inputEmail" class="sr-only">Identifiant</label>
-            <input name="pseudo" id="inputEmail" class="form-control" placeholder="Email address" required="" autofocus="">
-            <label for="inputPassword" class="sr-only">Password</label>
-            <input name="password" type="password" id="inputPassword" class="form-control" placeholder="Password" required="">
-            <div class="checkbox mb-3">
-                
-            </div>
-            <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
-            <p class="mt-5 mb-3 text-muted">© Boissons 2022 </p>
-        </form>
-  
+        <section class="vh-100">
+            <style>
+            body{
+                background-image:url('$path/Photos/imageLogin.jpeg');
+                background-repeat: no-repeat;
+                background-size: 100% 100%;
+            }
+            </style>
+            <div class="container py-5 h-100">
+              <div class="row d-flex justify-content-center align-items-center h-100">
+                <div class="col-12 col-md-8 col-lg-6 col-xl-5">
+                  <div class="card shadow-2-strong" style="border-radius: 1rem;">
+                    <div class="card-body p-5 text-center">
+          
+                    <body class="text-center" >
+                    <form class="form-signin" action="login" method="post">
 
-</body>
+                        <h1 class="h3 mb-3 font-weight-normal">Connexion</h1>
+
+                        <div class="form-outline mb-4">
+                          <label for="inputEmail" class="sr-only">Identifiant</label>
+                          <input name="pseudo" id="inputEmail" class="form-control" placeholder="Nom d'utilisateur" required="" autofocus="">
+                        </div>
+
+                        <div class="form-outline mb-4">
+                          <label for="inputPassword" class="sr-only">Password</label>
+                          <input name="password" type="password" id="inputPassword" class="form-control" placeholder="Mot de passe" required="">
+                          </div>
+                        <div class="checkbox mb-3">
+                
+                        </div>
+                        <button class="btn btn-lg btn-primary btn-block" type="submit">Connexion</button>
+                    </form>
+                    </body>
+          
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </section>
 END;
         return $temp;
     }
