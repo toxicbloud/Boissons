80869eca69cee7f660fdfecd59c12e91f03befa1
toxicbloud <fanta.r55000@gmail.com>
2022-11-29 20:49:10 +0100

    ajout de plein de truc de bdd

--- install.php
+++ install.php
@@ -3,7 +3,7 @@ include('Donnees.inc.php');
 $tab = parse_ini_file('conf/db.config.ini');
 $pdo = null;
 try {
-    $pdo = new PDO("mysql:host=".$tab['host'].";port=".$tab['port'].";charset=utf8mb4", $tab['username'], $tab['password']);
+    $pdo = new PDO("mysql:host=" . $tab['host'] . ";port=" . $tab['port'] . ";charset=utf8mb4", $tab['username'], $tab['password']);
 } catch (Exception $e) {
     echo $e->getMessage();
     print_r($e);
@@ -13,6 +13,9 @@ try {
 echo "<ul>";
 echo '<li>PDO connection OK</li>';
 echo "<br>";
+echo '<li>Creating database...</li>';
+$sql = "DROP DATABASE IF EXISTS " . $tab['database'];
+$pdo->exec($sql);
 $sql = "Create database if not exists " . $tab['database'];
 echo $pdo->query($sql) ? '<li>Database created</li>' : '<li>Database not created</li>';
 $sql = "use " . $tab['database'];
@@ -34,17 +37,86 @@ $sql = "CREATE TABLE IF NOT EXISTS user (
     phone VARCHAR(20) NULL
     );";
 
-echo $pdo->query($sql) ? '<li>Table user créée avec succès</li>' : 'USER KO';
+echo $pdo->query($sql) ? '<li>Table user crée avec succès</li>' : 'USER KO';
+
+// on créé des utilisateurs
+$sql = "INSERT INTO `user` (`username`, `password`, `firstname`, `lastname`, `email`) VALUES ('toxicbloud', '\$2y\$10\$VO9QS0hZEIE9gGn384DUROtXXMKmdtInYYAUS3hmaxMhvfQSpRGHy', 'Antonin', 'Rousseau', 'trucen@truc.fr') ";
+echo $pdo->query($sql) ? '<li>Utilisateur créé avec succès</li>' : 'USER KO';
+$sql = "DROP TABLE IF EXISTS cocktail";
+echo $pdo->query($sql) ? '<li>RESET table cocktail</li>' : 'COCKTAIL KO';
+$sql = "CREATE TABLE IF NOT EXISTS cocktail (
+    id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(60) NOT NULL,
+    ingredients VARCHAR(2000) NOT NULL,
+    preparation VARCHAR(2000) NOT NULL
+    );";
+echo $pdo->query($sql) ? '<li>Table cocktail crée avec succès</li>' : 'COCKTAIL KO';
+$sql = "DROP TABLE IF EXISTS Aliment";
+echo $pdo->query($sql) ? '<li>RESET table aliment</li>' : 'ALIMENT KO';
+$sql = "CREATE TABLE IF NOT EXISTS aliment (
+    id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(60) NOT NULL UNIQUE,
+    pere INT(6) UNSIGNED NULL,
+    FOREIGN KEY (pere) REFERENCES aliment(id)
+    );";
+echo $pdo->query($sql) ? '<li>Table aliment crée avec succès</li>' : 'ALIMENT KO';
+$sql = "DROP TABLE IF EXISTS compositionPanier";
+echo $pdo->query($sql) ? '<li>RESET table compositionPanier </li>' : 'PANIER KO';
+$sql = "CREATE TABLE IF NOT EXISTS compositionPanier (
+    id_user INT(6) UNSIGNED NOT NULL,
+    id_aliment INT(6) UNSIGNED NOT NULL,
+    dateAjout DATE,
+    PRIMARY KEY (id_user, id_aliment),
+    FOREIGN KEY (id_user) REFERENCES user(id),
+    FOREIGN KEY (id_aliment) REFERENCES aliment(id)
+    );";
+echo $pdo->query($sql) ? '<li>Table compositionPanier crée avec succès</li>' : 'PANIER KO';
+$sql = "DROP TABLE IF EXISTS compositionCocktail";
+echo $pdo->query($sql) ? '<li>RESET table compositionCocktail </li>' : 'COCKTAIL KO';
+$sql = "CREATE TABLE IF NOT EXISTS compositionCocktail (
+    id_cocktail INT(6) UNSIGNED NOT NULL,
+    id_aliment INT(6) UNSIGNED NOT NULL,
+    PRIMARY KEY (id_cocktail, id_aliment),
+    FOREIGN KEY (id_cocktail) REFERENCES cocktail(id),
+    FOREIGN KEY (id_aliment) REFERENCES aliment(id)
+    );";
+echo $pdo->query($sql) ? '<li>Table compositionCocktail crée avec succès</li>' : 'COCKTAIL KO';
 echo "</ul>";
-foreach($Recettes as $value){
-    $ing = $value['ingredients'];
-    echo "<li>";
-    echo $value['titre'];
-    $res = explode("|", $ing);
-    foreach($res as $str){
-        echo "<ul>";
-        echo $str;
-        echo "</ul>";
+foreach ($Recettes as $value) {
+    $prepare = $pdo->prepare("INSERT INTO cocktail (name, ingredients, preparation) VALUES (:name, :ingredients, :preparation)");
+    $prepare->bindValue(':name', $value['titre']);
+    $prepare->bindValue(':ingredients', $value['ingredients']);
+    $prepare->bindValue(':preparation', $value['preparation']);
+    echo $prepare->execute() ? '<li>Recette ' . $value['titre'] . ' ajoutée avec succès</li>' : 'Recette KO';
+}
+
+function existInDB($pdo, $name)
+{
+    $prepare = $pdo->prepare("SELECT * FROM aliment WHERE name = :name");
+    $prepare->bindValue(':name', $name);
+    $prepare->execute();
+    return $prepare->rowCount() > 0;
+}
+function getIdInDB($pdo, $name)
+{
+    $prepare = $pdo->prepare("SELECT id FROM aliment WHERE name = :name");
+    $prepare->bindValue(':name', $name);
+    $prepare->execute();
+    return $prepare->fetch()['id'];
+}
+
+foreach($Hierarchie as $key => $value){
+    $prepare = $pdo->prepare("INSERT INTO aliment (name) VALUES (:name)");
+    $prepare->bindValue(':name', $key);
+    $prepare->execute();
+}
+foreach($Hierarchie as $key => $value){
+    if(isset($value['super-categorie'])){
+        foreach($value['super-categorie'] as $superCategorie){
+            $prepare = $pdo->prepare("UPDATE aliment SET pere = :pere WHERE name = :name");
+            $prepare->bindValue(':name', $key);
+            $prepare->bindValue(':pere', getIdInDB($pdo, $superCategorie));
+            $prepare->execute();
+        }
     }
-    echo "</li>";
 }
