61987a92fe49541146f265d126fd0e017f5e207a
clement <clement.joly3@etu.univ-lorraine.fr>
2022-11-30 19:38:16 +0100

    Finalisation de loginView

--- src/views/LoginView.php
+++ src/views/LoginView.php
@@ -20,38 +20,36 @@ class LoginView
     {
         $path = $this->rq->getUri()->getBasePath();
         $temp = <<<END
+        <style>
+        body{
+            background-image:url('$path/Photos/imageLogin.jpeg');
+            background-repeat: no-repeat;
+            background-size: 100% 160%;
+        }
         <section class="vh-100">
-            <style>
-            body{
-                background-image:url('$path/Photos/imageLogin.jpeg');
-                background-repeat: no-repeat;
-                background-size: 100% 100%;
-            }
-            </style>
-            <div class="container py-5 h-100">
+        </style>
+            <div class="container py-5 h-200 mt-5">
               <div class="row d-flex justify-content-center align-items-center h-100">
                 <div class="col-12 col-md-8 col-lg-6 col-xl-5">
-                  <div class="card shadow-2-strong" style="border-radius: 1rem;">
+                  <div class="card shadow-2-strong" style="background-color: rgba(255, 255, 255, .7);">
                     <div class="card-body p-5 text-center">
           
                     <body class="text-center" >
                     <form class="form-signin" action="login" method="post">
 
-                        <h1 class="h3 mb-3 font-weight-normal">Connexion</h1>
+                        <h1 class="h3 mb-4" style="font-weight: bold; font-size: 250%; color: rgb(50, 50, 50);">Connexion</h1>
 
-                        <div class="form-outline mb-4">
+                        <div class="form-outline mb-5">
                           <label for="inputEmail" class="sr-only">Identifiant</label>
                           <input name="pseudo" id="inputEmail" class="form-control" placeholder="Nom d'utilisateur" required="" autofocus="">
                         </div>
 
-                        <div class="form-outline mb-4">
+                        <div class="form-outline mb-5">
                           <label for="inputPassword" class="sr-only">Password</label>
                           <input name="password" type="password" id="inputPassword" class="form-control" placeholder="Mot de passe" required="">
                           </div>
-                        <div class="checkbox mb-3">
-                
-                        </div>
-                        <button class="btn btn-lg btn-primary btn-block" type="submit">Connexion</button>
+
+                        <button class="btn btn-lg btn-secondary btn-block" type="submit" style="background-color: rgb(50, 50, 50);">Connexion</button>
                     </form>
                     </body>
           
